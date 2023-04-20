d26dd37206abf73e32121692d1bf9da163965df7
Rarikmilkrai Souza <96403081+rarikmilkrai@users.noreply.github.com>
2023-04-19 22:21:30 -0300

    Tipos Variaveis

---
+++ java-anatomia-classes/Tipos-Variaveis/src/App.java
@@ -0,0 +1,10 @@
+public class Tiposvariaveis {
+    public static void main(String[] args) throws Exception {
+       double salarioMinimo = 2500;
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+
+       final double VALOR_DE_PI = 3.14;
+    }
+}
