73b23e3d85314629efaf7b377ad65d0509b6d827
Rarikmilkrai Souza <96403081+rarikmilkrai@users.noreply.github.com>
2023-04-24 19:43:17 -0300

    Operadores Relacionais

---
+++ java-anatomia-classes/Tipos-Variaveis/Operadores/OperadoresRelacionais/src/OperadoresRelacionais.java
@@ -0,0 +1,60 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) throws Exception {
+        int 11 = 20;
+        int 12 = 20;
+        float f2 = 4.5f;
+        float f3 = 3.5f;
+        double d1 = 59.6d;
+        double d2 = 60.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "rarik";
+        String s2 = "Cicrano";
+        String s3 = "Fulano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 15978;
+        long l2 = 89766;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("11 == 12" + (11 == 12));
+        System.out.println("11 != 12" + (11 != 12));
+        System.out.println("11 > 12" + (11 > 12));
+        System.out.println("11 <= 12" + (11 <= 12));
+
+        System.out.println("f1 == f2" + (f1 == f2));
+        System.out.println("f1 != f2" + (f1 != f2));
+        System.out.println("f1 > f2" + (f1 > f2));
+        System.out.println("f1 <= f2" + (f1 <= f2));
+
+        System.out.println("c1 == c2" + (c1 == c2));
+        System.out.println("c1 != c2" + (c1 != c2));
+        System.out.println("c1 > c2" + (c1 > c2));
+        System.out.println("c1 <= c2" + (c1 <= c2));
+
+        System.out.println("s1 == s2" + (s1 == s2));
+        System.out.println("s1 == s3" + (s1 == s3));
+        System.out.println("s1 != s2" + (s1 != s2));
+        System.out.println("s1 > s2" + (s1 > s2));
+        System.out.println("s1 <= s2" + (s1 <= s2));
+
+        System.out.println("b1 == b2" + (b1 == b2));
+        System.out.println("b1 == b2" + (b1 == b3));
+        System.out.println("b1 != b2" + (b1 != b2));
+        System.out.println("b1 > b2" + (b1 > b2));
+        System.out.println("b1 <= b2" + (b1 <= b2));
+
+        System.out.println("f1 > f2" + (f1 > f2));
+
+
+        //AULA 2
+        
+
+
+
+
+    }
+
+}
