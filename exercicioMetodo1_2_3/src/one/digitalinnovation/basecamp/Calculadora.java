8541bc223bd23dd0a303f3a43aea31497755e870
Rarikmilkrai Souza <96403081+rarikmilkrai@users.noreply.github.com>
2023-05-03 21:09:50 -0300

    voltando aos estudos

---
+++ exercicioMetodo1_2_3/src/one/digitalinnovation/basecamp/Calculadora.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.basecamp;
+
+public class Calculadora {
+
+    public static void soma(int i, int j) {
+    }
+
+    public static void subtracao(int i, double d) {
+    }
+
+    public static void multiplicacao(int i, int j) {
+    }
+
+    public static void divisao(int i, double d) {
+    }
+
+}
